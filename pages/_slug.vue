a2d7a6863b78b0467c75eecc69eca039db5c08ad
Chris Perez <cp@reachpartners.ch>
2018-05-02 10:23:54 +0200

    testing around

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -20,7 +20,7 @@ export default {
   },
   asyncData (context) {
     // Check if we are in the editor mode
-    let version = context.query._storyblok || context.isDev ? 'draft' : 'published'
+    let version = context.query._storyblok || context.isDev ? 'draft' : 'draft'
 
     // Load the JSON from the API
     return context.app.$storyapi.get(`cdn/stories/${context.params.slug}`, {
